ee6c24b8437ceaa951e30444a439590d824b3c08
Eric Rochester <erochest@gmail.com>
2023-01-28 10:00:52 -0500

    Updated forth skels

--- forth/new/newday.py
+++ forth/new/newday.py
@@ -5,30 +5,31 @@ import click
 
 
 NEW_FILES = [
-    ('new/day.fth.mustache', 's/day%02d.fth'),
-    ('new/test-day.fth.mustache', 't/test-day%02d.fth'),
+    ('new/day.fth.mustache', 's/%04d/day%02d.fth'),
+    ('new/test-day.fth.mustache', 't/%04d/test-day%02d.fth'),
 ]
 
 APPEND_FILES = [
     ('new/Makefile.mustache', 'Makefile'),
 ]
 
-TOUCH_FILES = [
-    'd/day%02d.txt',
-    'f/day%02d.txt',
-]
-
 
 @click.command()
-@click.option('-d', '--day', required=True, type=int, help='The day to add to the project.')
-def main(day):
-    params = {'day': '%02d' % (day,)}
+@click.option('-y', '--year', default=202, type=int,
+              help='The year of Advent of Code. Default is 2020.')
+@click.option('-d', '--day', required=True, type=int,
+              help='The day to add to the project.')
+def main(year, day):
+    params = {
+        'year': '%04d' % (year,),
+        'day': '%02d' % (day,),
+        }
 
     for (src, dest) in NEW_FILES:
         with open(src) as f:
             output = chevron.render(f, params)
 
-        dest = dest % (day,) if '%' in dest else dest
+        dest = dest % (year, day) if '%' in dest else dest
         with open(dest, 'w') as fout:
             fout.write(output)
 
@@ -38,10 +39,6 @@ def main(day):
         with open(dest, 'a') as fout:
             fout.write('\n')
             fout.write(output)
-
-    for filename in TOUCH_FILES:
-        with open(filename % (day,), 'w') as f:
-            f.write('')
 
 
 if __name__ == '__main__':
