697bcb5a4be2299397f10fea8b7ea3711b1feeb9
Eric Rochester <erochest@gmail.com>
2023-01-27 21:19:35 -0500

    Integrated 2021, day 1

--- haskell/src/Advent.hs
+++ haskell/src/Advent.hs
@@ -1,14 +1,36 @@
 module Advent
     ( interact
     , interactAll
+    , interactLines
+    , interactFile
+    , Parseable(..)
+    , inspect
     ) where
 
 import qualified Data.ByteString.Builder    as B
 import qualified Data.ByteString.Lazy.Char8 as BS
+import           Data.Maybe (fromJust, mapMaybe)
 import           Prelude                    hiding (interact)
+
+import Debug.Trace
+
 
 interact :: ([BS.ByteString] -> B.Builder) -> IO ()
 interact f = BS.interact $ B.toLazyByteString . (<> B.charUtf8 '\n') . f . BS.lines
 
 interactAll :: (BS.ByteString -> B.Builder) -> IO ()
 interactAll f = BS.interact $ B.toLazyByteString . (<> B.charUtf8 '\n') . f
+
+inspect :: Show a => String -> a -> a
+inspect tag value = trace (tag ++ ": " ++ show value) value
+
+class Parseable a where
+  parse :: BS.ByteString -> Maybe a
+
+interactLines :: (Parseable a, Show b) => ([a] -> b) -> IO ()
+interactLines f =
+  BS.interact (B.toLazyByteString . (<> B.char8 '\n') . B.string8 . show . f . mapMaybe parse . BS.lines)
+
+interactFile :: (Parseable a, Show b) => (a -> b) -> IO ()
+interactFile f =
+  BS.interact (B.toLazyByteString . (<> B.char8 '\n') . B.string8 . show . f . fromJust . parse)
