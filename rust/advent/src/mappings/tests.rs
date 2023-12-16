b9beb8a84f4921ebc448104150d927ee029cc75b
Eric Rochester <erochest@gmail.com>
2023-12-16 11:30:33 -0500

    2023, day 05, part 2

--- rust/advent/src/mappings/tests.rs
+++ rust/advent/src/mappings/tests.rs
@@ -1,13 +1,41 @@
+use std::fs;
+
 use pretty_assertions::assert_eq;
 
-use super::{InputRange, MappingRange};
+use super::{get_minimum_location, get_minimum_location_range, InputRange, MappingRange};
+
+#[test]
+fn get_minimum_location_returns_correct_response() {
+    let input_file = "./../../sample/2023/day05.fixture";
+    let input = fs::read_to_string(input_file).unwrap();
+    let actual = get_minimum_location(&input);
+
+    assert!(actual.is_ok());
+    let actual = actual.unwrap();
+    assert!(actual.is_some());
+    let actual = actual.unwrap();
+    assert_eq!(actual, 35);
+}
+
+#[test]
+fn get_minimum_location_range_returns_correct_response() {
+    let input_file = "./../../sample/2023/day05.fixture";
+    let input = fs::read_to_string(input_file).unwrap();
+    let actual = get_minimum_location_range(&input);
+
+    assert!(actual.is_ok());
+    let actual = actual.unwrap();
+    assert!(actual.is_some());
+    let actual = actual.unwrap();
+    assert_eq!(actual, 46);
+}
 
 #[test]
 fn disjoint_range_application_changes_nothing() {
     let range_fn = MappingRange::new(50, 30, 20);
     let input = InputRange::new(10, 10);
 
-    let (unmatched, output) = range_fn.apply(input);
+    let (unmatched, output) = range_fn.apply(&input);
 
     assert_eq!(unmatched.len(), 1);
     assert_eq!(unmatched[0].start, 10);
@@ -22,7 +50,7 @@ fn embedded_range_application_matches_everything() {
     let range_fn = MappingRange::new(50, 30, 20);
     let input = InputRange::new(35, 10);
 
-    let (unmatched, output) = range_fn.apply(input);
+    let (unmatched, output) = range_fn.apply(&input);
 
     assert_eq!(unmatched.len(), 0);
 
@@ -38,7 +66,7 @@ fn overlapping_high_application_is_split() {
     let range_fn = MappingRange::new(50, 30, 20);
     let input = InputRange::new(40, 25);
 
-    let (unmatched, output) = range_fn.apply(input);
+    let (unmatched, output) = range_fn.apply(&input);
 
     assert_eq!(unmatched.len(), 1);
     assert_eq!(unmatched[0].start, 50);
@@ -57,7 +85,7 @@ fn overlapping_low_application_is_split() {
     let range_fn = MappingRange::new(50, 30, 20);
     let input = InputRange::new(20, 20);
 
-    let (unmatched, output) = range_fn.apply(input);
+    let (unmatched, output) = range_fn.apply(&input);
 
     assert_eq!(unmatched.len(), 1);
     assert_eq!(unmatched[0].start, 20);
@@ -76,7 +104,7 @@ fn reverse_embedded_application_is_split_three_ways() {
     let range_fn = MappingRange::new(50, 30, 20);
     let input = InputRange::new(20, 50);
 
-    let (unmatched, output) = range_fn.apply(input);
+    let (unmatched, output) = range_fn.apply(&input);
 
     assert_eq!(unmatched.len(), 2);
     assert_eq!(unmatched[0].start, 20);
