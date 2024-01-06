691c0fba2fdbc12abde08dbf44652a2820de98b4
Eric Rochester <erochest@gmail.com>
2024-01-06 11:45:29 -0500

    Rust, 2023.08.1

--- rust/advent/src/wasteland/test.rs
+++ rust/advent/src/wasteland/test.rs
@@ -12,12 +12,18 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)";
 
+static INPUT2: &str = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
 #[test]
 fn parse_input_path() {
     let result = parse_input(INPUT1);
     assert!(result.is_ok());
     let mapping = result.unwrap();
-    assert_eq!("RL".to_string(), mapping.path);
+    assert_eq!(b"RL".to_vec(), mapping.path);
 }
 
 #[test]
@@ -27,5 +33,23 @@ fn parse_input_network() {
     let mapping = result.unwrap();
     assert!(mapping.get("MMM").is_none());
     assert_eq!(Some(&Fork::new("ZZZ", "GGG")), mapping.get("CCC"));
+}
+
+#[test]
+fn path_to_end_single() {
+    let result = parse_input(INPUT1);
+    assert!(result.is_ok());
+    let mapping = result.unwrap();
+    let steps = mapping.steps_to_end("AAA");
+    assert_eq!(2, steps);
+}
+
+#[test]
+fn path_to_end_loop() {
+    let result = parse_input(INPUT2);
+    assert!(result.is_ok());
+    let mapping = result.unwrap();
+    let steps = mapping.steps_to_end("AAA");
+    assert_eq!(6, steps);
 }
 
